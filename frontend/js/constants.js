bfd0b39dc6f9974f5eca50e6b375f87972f0bccb
Xenotester <noob12460@gmail.com>
2022-04-27 10:46:35 +0800

    test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0xCB1Db3fbc274D65Ec08eb5E2A4D6228fcEAeC0C4"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
